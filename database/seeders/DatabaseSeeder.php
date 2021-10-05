66f58d5ac985858851ce1dee6252f9ca94d98e46
Hamzeh <Hamzeh.Al-tamimi@Hyperpay.com>
2021-10-05 11:49:30 +0300

    Backpack update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
     {
 //         \App\Models\User::factory(10)->create();
 
+
+
         DB::table('users')->insert([
             'name' => 'Amer',
             'email' => 'Amer@email.com',
